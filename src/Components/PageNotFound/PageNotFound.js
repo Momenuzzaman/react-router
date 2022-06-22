fd581db69e3e913e636388c5831cebfecbfd3b56
mdemon420 <mdeamon4@gmail.com>
2022-06-22 12:32:37 +0600

    add components

---
+++ src/Components/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <h1>PageNotFound</h1>
+            <h4>Don't ask me about this page</h4>
+        </div>
+    );
+};
+
+export default PageNotFound;
