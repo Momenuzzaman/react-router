9e57d3399f1dfdcf07409fae9ed544acfb545396
mdemon420 <mdeamon4@gmail.com>
2022-06-24 17:58:15 +0600

    add button

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,17 +1,22 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Friend.css';
 
 const Friend = (props) => {
     console.log(props);
     const {name,phone,address,company,id} = props.friend;
     const url = `/friend/${id}`;
+    const navigate = useNavigate();
+    const handleOnClick = () =>{
+        navigate(url)
+    }
     return (
         <div className="friend-container">
             <h3>Name: {name}</h3>
             <h3>Phone:{phone}</h3>
             <p>Address: {address.street}</p>
             <p>Company: {company.name}</p>
-            <Link to={url}>Detail</Link>
+            {/* <Link to={url}>Detail</Link> */}
+            <button onClick={handleOnClick}>Show detail</button>
         </div>
     );
 };
